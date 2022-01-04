578ad415d468aa4a18d45bb0910fb9a51b66cc14
svworld01 <saurabh.verma@enveu.com>
2022-01-05 01:47:52 +0530

    adding delay in consumer

--- src/main/java/com/saurabh/rappidmq/consumer/OrderConsumer.java
+++ src/main/java/com/saurabh/rappidmq/consumer/OrderConsumer.java
@@ -6,11 +6,14 @@ import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class OrderConsumer {
 
     @RabbitListener(queues = MessagingConfig.QUEUE)
-    public void consumeMessageFromQueue(OrderStatus orderStatus){
+    public void consumeMessageFromQueue(OrderStatus orderStatus) throws InterruptedException {
+        TimeUnit.MINUTES.sleep(1);
         System.out.println("Message Received from Queue : "+ orderStatus);
     }
 }
